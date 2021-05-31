62af133a1a0289841c942310eb4bab8faf275010
Anandkumar08 <81465890+Anandkumar08@users.noreply.github.com>
2021-06-01 00:40:38 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,75 @@
+(function($) {
+
+    "use strict";
+
+    $(window).on('load', function() {
+
+        /* 
+     MixitUp
+     ========================================================================== */
+        $('#portfolio').mixItUp();
+
+        /* 
+
+        /*Page Loader active
+          ========================================================*/
+        $('#preloader').fadeOut();
+
+        // Sticky Nav
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 200) {
+                $('.scrolling-navbar').addClass('top-nav-collapse');
+            } else {
+                $('.scrolling-navbar').removeClass('top-nav-collapse');
+            }
+        });
+
+        /* slicknav mobile menu active  */
+        $('.mobile-menu').slicknav({
+            prependTo: '.navbar-header',
+            parentTag: 'liner',
+            allowParentLinks: true,
+            duplicate: true,
+            label: '',
+            closedSymbol: '<i class="icon-arrow-right"></i>',
+            openedSymbol: '<i class="icon-arrow-down"></i>',
+        });
+
+
+        /* Back Top Link active
+        ========================================================*/
+        var offset = 200;
+        var duration = 500;
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.back-to-top').fadeIn(400);
+            } else {
+                $('.back-to-top').fadeOut(400);
+            }
+        });
+
+        $('.back-to-top').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+
+
+
+    });
+
+
+}(jQuery));
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {
+    myFunction()
+};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
